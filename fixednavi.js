e5069096d3b502520b4a10b1c23ec11e0bf392eb
kalipinna <77393980+kalipinna@users.noreply.github.com>
2021-08-20 22:00:48 +0200

    Add files via upload

---
+++ fixednavi.js
@@ -0,0 +1,31 @@
+(function() {
+
+  let topNavi = $("#navigation").offset().top;
+  let topNaviHeight = $("#navigation").height() + 10;
+
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop() >= topNavi) {
+      $("#navigation").css({
+        position: "fixed",
+        backgroundImage: "url('https://static.tumblr.com/khncf5z/wmgqxohwv/lago_modificato.jpg')",
+      });
+      $("#stuff").css({
+        paddingTop: topNaviHeight
+      });
+
+    } else {
+      $("#navigation").css({
+        position: "static",
+        backgroundImage: "url('https://static.tumblr.com/khncf5z/wmgqxohwv/lago_modificato.jpg')",
+        backgroundPosition: "top",
+        backgroundSize: "cover",
+        backgroundAttachment: "fixed"
+      });
+
+      $("#stuff").css({
+        paddingTop: "0"
+      });
+    }
+  });
+}());
